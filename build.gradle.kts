78e4766eab9a3d08672262c2fb224d6410c028d0
Stefan Lobbenmeier <stefan.lobbenmeier@objego.de>
2024-12-12 10:41:23 +0100

    remove db

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,11 +23,6 @@ dependencyManagement {
 dependencies {
 
     implementation("org.springframework.boot:spring-boot-starter-web")
-    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-    implementation("org.springframework.boot:spring-boot-starter-validation")
-    implementation("org.springframework.boot:spring-boot-starter-webflux")
-    implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
-    implementation("org.springframework.boot:spring-boot-starter-actuator")
 
     implementation("org.springframework.cloud:spring-cloud-starter-stream-kafka")
 }
